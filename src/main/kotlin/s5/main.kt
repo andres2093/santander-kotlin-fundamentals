dd6ecdb7be060a8fd95a4977db3b85e0dee4d5d6
Andres Ramirez <andres200493@gmail.com>
2021-06-29 21:24:33 -0500

    R1 -> S5

--- src/main/kotlin/s5/main.kt
+++ src/main/kotlin/s5/main.kt
@@ -54,5 +54,41 @@ fun main() {
     fun suma(a: Int, b: Int) = a + b
     val suma = calculadora(10, 5, ::suma)
 
-    println(suma)
+//    println(suma)
+
+//    Reto 1
+    getPromo(1000.0, "RORO")
+    getPromo(1000.0, "PROMO2020")
+    getPromo(1000.0, "NOIVA")
+    getPromo(1000.0, "MINUS100")
+    getPromo(1000.0, "HALFIVA")
+}
+
+fun getPromo(price: Double, coupon: String) {
+
+    val getPrice: (Double, (Double) -> Double) -> Unit = { price, operation ->
+        println("El precio actual de tu producto es ${operation(price)}")
+    }
+
+    val totalPrice = { price: Double -> price * 1.16 }
+    val halfIva = { price: Double -> price * 1.08 }
+    val noIva = { price: Double -> price }
+    val ranges = { price: Double ->
+        when (price) {
+            in 0.0..1000.0 -> price * 1.12
+            in 1000.0..2000.0 -> price * 1.04
+            in 2000.0..4000.0 -> (price * 1.16) / 2
+            else -> price / 3
+        }
+    }
+    val minus100 = { price: Double -> price * 1.16 - 100 }
+
+
+    when (coupon) {
+        "PROMO2020" -> getPrice(price, ranges)
+        "NOIVA" -> getPrice(price, noIva)
+        "MINUS100" -> getPrice(price, minus100)
+        "HALFIVA" -> getPrice(price, halfIva)
+        else -> getPrice(price, totalPrice)
+    }
 }
