7bc107bd30a84a460c11f5cb81f26014907fe470
Andres Ramirez <andres200493@gmail.com>
2021-06-29 22:25:20 -0500

    R2 -> S5

--- src/main/kotlin/s5/e3.kt
+++ src/main/kotlin/s5/e3.kt
@@ -22,9 +22,29 @@ fun main() {
 //    val porNombre = notasAlumnos.filter { (key, value) ->  value == 6}
 //    println(porNombre)
 
-    val preciosLibres = setOf(1024.0, 2408.5, 8224.33)
-    val preciosFinales = preciosLibres.map { it * 1.16 }
-    println(preciosFinales)
+//    val preciosLibres = setOf(1024.0, 2408.5, 8224.33)
+//    val preciosFinales = preciosLibres.map { it * 1.16 }
+//    println(preciosFinales)
 
+//    val aprobados = calificaciones.filter { it > 15 }
+//    println(aprobados.size == 0)
+//    println(aprobados.isEmpty())
+
+    //    Reto 2
+    val calificacionesR2 = listOf(10,8, 9 ,5,3,2,5,1,7,6,9,4,10,3,6,2,6,5)
+
+    //Sacar el promedio de la clase
+    val promedio = calificacionesR2.sum().toDouble()/calificacionesR2.size
+    val promedioAvg = calificacionesR2.average()
+    println("promedio: $promedio | promedioAvg: $promedioAvg")
+
+    println(calificacionesR2)
+
+    //Verificar que ningún alumno halla sacado menos de 4
+    println("Los alumnos sacaron menos de 4: ${!calificacionesR2.none{it < 4}}")
+
+    //Subir puntos extra pero no permitir que alguien tenga 11 en adelante
+    val califExtra = calificacionesR2.map { if (it>=10) it else it + 1 }
+    println("Las calificaciones con punto extra son: $califExtra")
 
 }
