1386205dd41218c382d95eb23527da2863cdacfa
Andres Ramirez <andres200493@gmail.com>
2021-07-06 19:56:02 -0500

    R1 -> S7

---
+++ src/main/kotlin/s7/r1/main.kt
@@ -0,0 +1,40 @@
+package s7.r1
+
+fun main() {
+    val names: List<String?> = listOf("Name One", null, "Name Three")
+
+    // .1
+    // Por Condiciones
+    names.forEach {
+        if (it != null) {
+            println(it)
+        }
+    }
+    // Por safe call
+    names.forEach { it?.let { println(it) } }
+    println("--------------------")
+    // .2
+    //imprimir las casillas nulas
+    names.forEach {
+        val printString = it ?: "No se encontró nombre en esta casilla"
+        println(printString)
+    }
+    println("--------------------")
+    // .3
+    val person = Person("Andres")
+    println(person.getLastName())
+    println(person.getFullName())
+}
+class Person(
+    private val name:String,
+    private val lastName:String? = null){
+
+    fun getFullName(): String {
+        val lName =  lastName ?: throw IllegalArgumentException("Necesitas apellidos")
+        return "$name $lName"
+    }
+    fun getLastName() : String {
+        val lName = lastName ?: return "No se encontró apellido"
+        return lName
+    }
+}
